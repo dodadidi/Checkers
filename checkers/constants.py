3f921428e6fa28763bb4ea0bfd601ca9b161b6f8
Ariel Hadad <48453004+arieell25@users.noreply.github.com>
2021-02-27 11:33:04 +0200

    gui style

--- checkers/constants.py
+++ checkers/constants.py
@@ -1,8 +1,9 @@
-#all the constats values will be here
+########### file of all the constats values ########### 
 import pygame
-WIDTH, HEIGHT = 700,700 #8 hundreds pixels 
-ROWS, COLS = 8,8
-SQUARE_SIZE = WIDTH//COLS
+
+WIDTH, HEIGHT = 700,700 #pixels 
+ROWS, COLS = 8,8 #number of rows and cols of board
+SQUARE_SIZE = WIDTH//COLS #size of each square in board
 
 #rgb
 RED = (255, 0, 0)
@@ -10,6 +11,16 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GRAY = (128,128,128,128)
+YELLOW = (255, 204, 0)
+
+LIGHT_BLUE = (51, 153, 255)
+DARK_BLUE = (0, 38, 77)
+PINK = (204, 0, 153)
+DARK_PINK = (128, 0, 94)
+LIGHT_PINK = (255, 179, 235)
+ORANGE = (255, 140, 26)
+PITCH = (255, 153, 153)
+
 
 #bring and resize crown image
-CROWN = pygame.transform.scale(pygame.image.load('assets/crown.png'),(40,21))
+CROWN = pygame.transform.scale(pygame.image.load('images/crown.png'),(40,21))
