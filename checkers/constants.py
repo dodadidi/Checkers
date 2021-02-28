de3c0f4dd762f8f7712c5eba1c106c79ead64113
Ariel Hadad <48453004+arieell25@users.noreply.github.com>
2021-02-28 14:26:23 +0200

    finish board

--- checkers/constants.py
+++ checkers/constants.py
@@ -11,16 +11,18 @@ WHITE = (255, 255, 255)
 BLACK = (0, 0, 0)
 BLUE = (0, 0, 255)
 GRAY = (128,128,128,128)
+
+##########################
 YELLOW = (255, 204, 0)
-
-LIGHT_BLUE = (51, 153, 255)
-DARK_BLUE = (0, 38, 77)
-PINK = (204, 0, 153)
-DARK_PINK = (128, 0, 94)
-LIGHT_PINK = (255, 179, 235)
-ORANGE = (255, 140, 26)
+#DELETE
+# LIGHT_BLUE = (51, 153, 255)
+# DARK_BLUE = (0, 38, 77)
+# PINK = (204, 0, 153)
+# DARK_PINK = (128, 0, 94)
+# LIGHT_PINK = (255, 179, 235)
+# ORANGE = (255, 140, 26)
 PITCH = (255, 153, 153)
-
+##########################
 
 #bring and resize crown image
 CROWN = pygame.transform.scale(pygame.image.load('images/crown.png'),(40,21))
--- checkers/piece.py
+++ checkers/piece.py
@@ -1,8 +1,8 @@
-from .constants import RED, WHITE, SQUARE_SIZE, GRAY, CROWN
+from .constants import RED, WHITE, SQUARE_SIZE, GRAY, CROWN, GRAY
 import pygame
 
 class Piece:
-    PADDING = 10
+    PADDING = 15
     OUTLINE = 2
 
     def __init__(self, row, col, color):
@@ -10,35 +10,38 @@ class Piece:
         self.col = col
         self.color = color
         self.king = False
-
-        if self.color == RED:
-            self.direction = -1
-        else:
-            self.direction = 1
-
         self.x = 0
         self.y = 0
-        self.calc_pos()
+        self.calculate_curr_pos()
     
-    def calc_pos(self):
+    def calculate_curr_pos(self):
         self.x = SQUARE_SIZE * self.col + SQUARE_SIZE // 2
         self.y = SQUARE_SIZE * self.row + SQUARE_SIZE // 2
 
-    def make_king(self):
+    def change_to_king(self):
         self.king = True
     
-    def draw(self, win):
+	#the following draws a piece on the board.
+    def draw(self, win,selected):
+        """ 
+        draw piece as a circle with the right color and gray border at the middle of square
+        """
         radius = SQUARE_SIZE // 2 - self.PADDING
-        pygame.draw.circle(win, GRAY, (self.x, self.y), radius + self.OUTLINE)
         pygame.draw.circle(win, self.color, (self.x, self.y), radius)
         if self.king:
             win.blit(CROWN, (self.x - CROWN.get_width()//2, self.y - CROWN.get_height()//2))
 
     def move(self, row, col):
+        """
+        update piece curr_pos and call to calculate_curr_pos()
+        """
         self.row = row
         self.col = col
-        self.calc_pos()
+        self.calculate_curr_pos()
         
+    #def get_piece_color(self,row,col):
+     #   return self.color
+   
 #for debugging
     def __repr__(self):
         return str(self.color)
